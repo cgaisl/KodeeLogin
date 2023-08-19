1376bb889d62e3fc372f336251d0f4b2c182f012
Christian Gaisl <christian.gaisl@gmail.com>
2023-08-19 16:29:04 +0200

    add password hidden state

--- common/src/commonMain/kotlin/at/cgaisl/common/App.kt
+++ common/src/commonMain/kotlin/at/cgaisl/common/App.kt
@@ -9,11 +9,18 @@ import androidx.compose.runtime.*
 @Composable
 internal fun App() {
     var progress by remember { mutableStateOf(0f) }
+    var x by remember { mutableStateOf(0f) }
+    var y by remember { mutableStateOf(0f) }
+    var rot by remember { mutableStateOf(0f) }
 
     Column {
 
         Kodee(
             kodeeState = KodeeState.EmailInput(progress = progress)
+        )
+
+        Kodee(
+            kodeeState = KodeeState.PasswordInputHidden, x, y, rot
         )
 
         Slider(
@@ -24,6 +31,37 @@ internal fun App() {
             valueRange = (0f.. 1f),
         )
 
+        Slider(
+            value = x,
+            onValueChange = { newValue ->
+                x = newValue
+            },
+            valueRange = (-500f.. 0f),
+        )
+
+        Text("x Slider Value: $x")
+
+        Slider(
+            value = y,
+            onValueChange = { newValue ->
+                y = newValue
+            },
+            valueRange = (0f.. 500f),
+        )
+
+        Text("y Slider Value: $y")
+
+        Slider(
+            value = rot,
+            onValueChange = { newValue ->
+                rot = newValue
+            },
+            valueRange = (-500f.. 0f),
+        )
+
+        Text("rot Slider Value: $rot")
+
         Text("progress Slider Value: $progress")
+
     }
 }
--- common/src/commonMain/kotlin/at/cgaisl/common/Kodee.kt
+++ common/src/commonMain/kotlin/at/cgaisl/common/Kodee.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -11,9 +12,9 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import kotlinx.coroutines.delay
 import kotlin.random.Random
-
 
 sealed class KodeeState(
     val headRotationX: Float = 0f,
@@ -24,11 +25,32 @@ sealed class KodeeState(
     val faceRotationZ: Float = 0f,
     val faceTranslationY: Float = 0f,
     val faceTranslationX: Float = 0f,
+    val armsTranslationX: Float = 0f,
+    val armsRotationY: Float = 0f,
+    val armLeftRotationZ: Float = 0f,
+    val armLeftTranslationX: Float = 0f,
+    val armLeftTranslationY: Float = 0f,
+    val armRightRotationZ: Float = 0f,
+    val armRightTranslationX: Float = 0f,
+    val armRightTranslationY: Float = 0f,
+    val isBlinking: Boolean = true,
 ) {
     object Idle : KodeeState()
-    object PasswordInputShown : KodeeState()
-    object PasswordInputHidden : KodeeState()
-    data class EmailInput(val progress: Float) : KodeeState(
+    object PasswordInputShown : KodeeState(
+        isBlinking = false,
+        armsTranslationX = 00f
+    )
+
+    object PasswordInputHidden : KodeeState(
+        isBlinking = false,
+        armLeftTranslationX = 84f,
+        armLeftRotationZ = 158f,
+        armRightTranslationX = -101f,
+        armRightTranslationY = 15f,
+        armRightRotationZ = -150f,
+    )
+
+    class EmailInput(progress: Float) : KodeeState(
         headRotationX = -20f,
         headRotationY = -20f + progress * 40f,
         headRotationZ = -30f + progress * 60f,
@@ -45,17 +67,21 @@ sealed class KodeeState(
         } else {
             -20f * (2f * progress - 1f)
         },
+        armsTranslationX = -(-30f + progress * 60f),
+        armsRotationY = -20f + progress * 40f,
     )
 }
 
 @Composable
 fun Kodee(
     kodeeState: KodeeState,
+    x: Float = 0f,
+    y: Float = 0f,
+    rot: Float = 0f,
 ) {
     val conversion = with(LocalDensity.current) {
         1.dp.toPx()
     }
-
 
     fun Int.px(): Dp {
         return (this / conversion).toInt().dp
@@ -73,7 +99,9 @@ fun Kodee(
         }
     )
 
-    LaunchedEffect(Unit) {
+    LaunchedEffect(kodeeState.isBlinking) {
+        if (!kodeeState.isBlinking) return@LaunchedEffect
+
         while (true) {
             isBlinking = true
             delay((Random.nextInt(1, 10) * 1000L))
@@ -81,22 +109,52 @@ fun Kodee(
     }
 
     Box {
-        ResourceImage(
-            "arms_regular.png",
-            modifier = Modifier
-                .padding(top = 87.px(), start = 0.px())
+        // Arms
+        Row(
+            modifier = Modifier.zIndex(
+                when (kodeeState) {
+                    KodeeState.PasswordInputHidden, KodeeState.PasswordInputShown -> 1f
+                    is KodeeState.EmailInput, KodeeState.Idle -> 0f
+                }
+            )
+                .padding(top = 117.px(), start = 0.px())
                 .graphicsLayer(
-                    translationX = -kodeeState.headRotationZ,
-                    rotationY = kodeeState.headRotationY / 3
+                    translationX = kodeeState.armsTranslationX,
+                    rotationY = kodeeState.armsRotationY,
                 )
+        ) {
+            ResourceImage(
+                "arm_left.png",
+                modifier = Modifier
+                    .padding(top = 42.px())
+                    .graphicsLayer(
+                        rotationZ = kodeeState.armLeftRotationZ,
+                        translationX = kodeeState.armLeftTranslationX,
+                        translationY = kodeeState.armLeftTranslationY,
+                    )
+            )
 
-        )
+            ResourceImage(
+                "arm_right.png",
+                modifier = Modifier
+                    .padding(start = 271.px())
+                    .graphicsLayer(
+                        rotationZ = kodeeState.armRightRotationZ,
+                        translationX = kodeeState.armRightTranslationX,
+                        translationY = kodeeState.armRightTranslationY,
+                    )
+            )
+        }
+
+
+        // Legs
         ResourceImage(
             "legs.png",
             modifier = Modifier.padding(top = 247.px(), start = 262.px())
 
         )
 
+        // Head
         ResourceImage(
             "head.png",
             modifier = Modifier
@@ -109,6 +167,7 @@ fun Kodee(
 
         )
 
+        // Face
         Box(
             modifier = Modifier
                 .padding(top = 131.px(), start = 223.px())
