aca37a62c51fa27a7252597d40bb479d2a3744ce
Christian Gaisl <christian.gaisl@gmail.com>
2023-08-19 21:41:59 +0200

    Expand background surface

--- common/src/commonMain/kotlin/at/cgaisl/common/Navigation.kt
+++ common/src/commonMain/kotlin/at/cgaisl/common/Navigation.kt
@@ -2,30 +2,34 @@ package at.cgaisl.common
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 
 @Composable
 fun Navigation() {
     var currentScreen by remember { mutableStateOf(Screens.Login) }
 
 
-    Column {
-        Row {
-            Screens.entries.forEach { screen ->
-                Button(onClick = { currentScreen = screen }) {
-                    Text(screen.name)
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column {
+            Row {
+                Screens.entries.forEach { screen ->
+                    Button(onClick = { currentScreen = screen }) {
+                        Text(screen.name)
+                    }
                 }
             }
-        }
 
-        Surface {
+
             when (currentScreen) {
                 Screens.Playground -> KodeePlayground()
                 Screens.Login -> KodeeLogin()
             }
+
         }
     }
 }
